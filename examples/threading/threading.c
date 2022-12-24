83c3c9d28a6cf8d94a752fbbc993280bea044503
btlad <88908101+btlad@users.noreply.github.com>
2022-12-24 13:53:00 +0200

    assignment4 part1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -10,10 +11,38 @@
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int ret;
+    struct thread_data *thread_func_args = (struct thread_data *)thread_param;
+
+    thread_func_args->thread_complete_success = false;
+
+    errno = 0;
+    usleep(1000*thread_func_args->thread_wait_to_obtain_ms);
+    if (errno) {
+        perror ("usleep");
+        pthread_exit(thread_param);
+    }
+
+    ret = pthread_mutex_lock(thread_func_args->thread_mutex);
+   if (ret) {
+        errno = ret;
+        perror("pthread_mutex_lock");
+        pthread_exit(thread_param);
+    }
+
+    errno = 0;
+    usleep(1000*thread_func_args->thread_wait_to_release_ms);
+    if (errno) {
+        perror ("usleep");
+        pthread_exit(thread_param);
+    }
+
+    pthread_mutex_unlock(thread_func_args->thread_mutex);
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +57,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    struct thread_data *th_data = NULL;
+    int ret;
 
+    th_data = (struct thread_data *)calloc(1, sizeof(struct thread_data));
+    if (!th_data) {
+        perror ("calloc");
+        return false;
+    }
+
+    th_data->thread_mutex = mutex;
+    th_data->thread_wait_to_obtain_ms = wait_to_obtain_ms;
+    th_data->thread_wait_to_release_ms = wait_to_release_ms;
+    th_data->thread_complete_success = false;
+
+    ret = pthread_create(thread, NULL, threadfunc, th_data);
+    if (ret) {
+        errno = ret;
+        perror("pthread_create");
+        return false;
+    }
+
+    return true;
+}
