bfc4761a13ef8fcea3440b32321873cb8578f62a
btlad <88908101+btlad@users.noreply.github.com>
2023-02-22 23:39:20 +0200

    Assignment 7 part 1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -32,7 +32,49 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
-    return NULL;
+	uint8_t index;
+	struct aesd_buffer_entry *entry_p;
+	uint32_t n_chars = 0;
+
+    if (buffer->full) {
+		AESD_CIRCULAR_BUFFER_FOREACH(entry_p,buffer,index) {
+			n_chars += entry_p->size;
+		}
+	}
+	else {
+		AESD_CIRCULAR_BUFFER_FOREACH(entry_p,buffer,index) {
+
+			if (buffer->out_offs == buffer->in_offs) {
+				break;
+			}
+
+			if (
+			    ((buffer->out_offs < buffer->in_offs) &&
+			     ((index >= buffer->out_offs) && (index < buffer->in_offs)))
+			     ||
+			    ((buffer->out_offs > buffer->in_offs) &&
+			     ((index  < buffer->in_offs) || (index >= buffer->out_offs)))
+			    ) {
+
+				n_chars += entry_p->size;
+			}
+		}
+	}
+	
+	if ((n_chars == 0) || ((n_chars - 1) < char_offset)) {
+		return NULL;
+	}
+	else {
+		for (index = 0; index<AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; index++) {
+			entry_p = &((buffer)->entry[(index + buffer->out_offs) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED]);
+			if (entry_p->size > char_offset) {
+				break;
+			}
+			char_offset -= entry_p->size;
+		}
+		*entry_offset_byte_rtn = char_offset;
+        return entry_p;
+	}
 }
 
 /**
@@ -47,6 +89,22 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+
+    if (buffer->full) {
+		buffer->out_offs++;
+		buffer->out_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	}
+
+	/* Try buffer->entry[buffer->in_offs] = *add_entry */
+	buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+	buffer->entry[buffer->in_offs].size = add_entry->size;
+
+	buffer->in_offs++;
+	buffer->in_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+	if (buffer->in_offs == buffer->out_offs) {
+		buffer->full = true;
+	}
 }
 
 /**
